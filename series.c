7ecdfa4cf6317a9ea4feca53c7ace5009a579c14
Samiul islam sami <samiulislam07186@gmail.com>
2024-10-27 23:16:11 +0600

    Add files via upload

---
+++ series.c
@@ -0,0 +1,331 @@
+
+/*7.1
+#include<stdio.h>
+int main()
+{
+    int i, sum=0, n;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for( i=1; i<=n; i++){
+        sum += i;
+    }
+     printf("Sum = %d\n", sum);
+    return 0;
+}
+*/
+/*7.2
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += i*2;
+    }
+    printf("Sum =%d", sum);
+
+    return 0;
+}
+*/
+/*7.3
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+    {
+        sum += 2 * i+ 1;
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.4
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += 8 * i- 4;
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.5
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += 3 *i-1;
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+//OR
+/*
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+    {
+        sum += 2+i*3;
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.6
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += i*(i+1);
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+
+/*7.7
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum +=(3 * i - 1) * (2 * i - 1);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.8
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum +=(2 * i - 1) * (2 * i + 1);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.9
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += (2*i-1)*(2*i-1);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.10
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += i*(2*i-1)*(2*i-1);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.11
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += i*(i+1)*(i+1);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.12
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += (i*i)*(i+1)*(i+1);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.13
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += i*(i+1)*(i+2);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.14
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += (2*i-1)*(2*i+1)*(2*i+3)*(2*i+5);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.15
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += (3*i-1)*(3*i+2)*(3*i+5);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
+/*7.16
+#include<stdio.h>
+int main()
+
+{
+    int n, sum=0;
+    printf("Enter nth term:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        sum += (i+4)*(i+5)*(i+6);
+
+    }
+    printf("Sum = %d\n", sum);
+
+
+    return 0;
+}
+*/
